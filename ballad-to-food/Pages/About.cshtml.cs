c0065a3f6b555e09873a603d4196fef03bfd0ed9
Octavio Martinez <o.martinez.gar@gmail.com>
2021-02-03 21:11:10 +0100

    Initial Commit

---
+++ ballad-to-food/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ballad_to_food.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ballad-to-food/Pages/Contact.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ballad_to_food.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
