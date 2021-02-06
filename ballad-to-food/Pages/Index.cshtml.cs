f0dbd5b447d29610a8f45a46b7df200d4e38db04
Octavio Martinez <o.martinez.gar@gmail.com>
2021-02-06 16:38:51 +0100

    load data from app settings

--- ballad-to-food/Pages/Index.cshtml.cs
+++ ballad-to-food/Pages/Index.cshtml.cs
@@ -1,25 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ballad_to_food.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+
+        private readonly IConfiguration _config;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _config = configuration;
         }
 
         public void OnGet()
         {
-
+            Message = _config["Message"];
         }
     }
 }
